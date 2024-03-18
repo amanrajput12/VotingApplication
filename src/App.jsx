9bfeddd2e309c2bd78553433ca1810f2a07d5431
Aman Rajput <rajputaman724857@gmail.com>
2024-03-18 13:09:41 +0530

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2 className='bg-gray-950 text-pink-300'>Hello</h2>
+    </>
+  )
+}
+
+export default App
